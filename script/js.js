af62a030577bdeeb3886e2b6f2b06ce12984c110
Liubomyr <pylypiv.liubomyr@gmail.com>
2018-06-07 23:05:53 +0300

    jQuery add

---
+++ script/js.js
@@ -0,0 +1,5 @@
+    $('#Table').append(  '<table>' );
+     for(i=0;i<3;i++){
+        $('#Table').append( '<tr><td>' + 'result' +  i + '</td></tr>' );
+    }
+     $('#Table').append(  '</table>' );
